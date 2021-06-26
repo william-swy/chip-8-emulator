0a6776ab82fb2791e5f472c4baf760fb2c629b38
BigBlue <shenpaiwilliam@gmail.com>
2021-06-26 11:13:46 -0600

    Add project files.

---
+++ chip8_emulator/chip8_emulator.cpp
@@ -0,0 +1,12 @@
+﻿// chip8_emulator.cpp : Defines the entry point for the application.
+//
+
+#include "chip8_emulator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
