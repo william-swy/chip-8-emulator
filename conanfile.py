749d6d47c9d67170f81ba3d754fdd203bcb42500
William Shen <shenpaiwilliam@gmail.com>
2023-08-21 22:55:13 -0700

    Fix libconv version conflicts on linux

--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,19 @@
+from sys import platform
+
 from conans import ConanFile
 
 
 class CHIP8Emulator(ConanFile):
     name = "CHIP8Emulator"
     version = "0.2.0"
-    requires = (
-        "catch2/2.13.7",
-        "sdl2/2.0.16@bincrafters/stable",
-    )
     generators = "cmake", "cmake_find_package"
 
     def package_info(self):
         self.conf_info.define("tools.system.package_manager:mode", "install");
         self.conf_info.define("tools.system.package_manager:sudo", True)
+
+    def requirements(self):
+        self.requires("catch2/2.13.7")
+        self.requires("sdl2/2.0.16@bincrafters/stable")
+        if platform == "linux" or platform == "linux2":
+            self.requires("libiconv/1.17", override=True)
